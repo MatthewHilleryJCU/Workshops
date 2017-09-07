efd0594c38ca41a5a7d58ea6c98bce30a8bce154
Matt <matthew.hillery@my.jcu.edu.au>
2017-09-07 21:42:22 +1000

    security checker and lottery generator :)

---
+++ Workshop05/Lottery_ticket_generator.py
@@ -0,0 +1,13 @@
+from random import *
+
+number_picks = int (input("How many quick picks? "))
+numbers = []
+
+
+numbers1 = str(randint(range(1, 45), 6))
+numbers.append(numbers1)
+
+print(numbers)
+
+
+
