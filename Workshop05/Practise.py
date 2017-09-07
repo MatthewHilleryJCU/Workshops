80b4ddb146e8cfd38c7edb821650fe658ef2df43
Matt <matthew.hillery@my.jcu.edu.au>
2017-09-07 12:54:30 +1000

    practise

--- Workshop05/Practise.py
+++ Workshop05/Practise.py
@@ -1,8 +1,8 @@
-def frange(start, stop, step):
+def num_range(start, stop, step):
 
     while start < stop:
         yield start
         start += step
 
-for i in frange(0.5, 1.0, 0.1):
+for i in num_range(0.5, 1.0, 0.1):
         print(i)
